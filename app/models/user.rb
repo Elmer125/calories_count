b27c2a5b4be6159315f629c91a3a97566389be17
Elmer125 <elmermauricio05@hotmail.com>
2022-10-10 17:25:55 -0500

    Adds active_storage and avatar a devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :calories
   validates_presence_of :name, length: { minimum: 5, maximum: 40 }
+  has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.png')),
+      filename: 'default_profile.png',
+      content_type: 'image/png'
+    )
+  end
 end
