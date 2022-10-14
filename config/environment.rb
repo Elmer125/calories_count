7d486fd1fef8e7f1eb7625028abf6525db9807ae
Elmer125 <elmermauricio05@hotmail.com>
2022-10-14 12:37:09 -0500

    change config environment

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: Rails.application.credentials.config[:SENDGRID_USERNAME], # This is the string literal 'apikey', NOT the ID of your API key
-  password: Rails.application.credentials.config[:SENDGRID_PASSWORD], # This is the secret sendgrid API key which was issued during API key creation
-  domain: 'heroku.com',
+  user_name: ENV['SENDGRID_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
+  password: ENV['SENDGRID_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
+  domain: 'infinite-plateau-19302.herokuapp.com',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
