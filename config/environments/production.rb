194e43d0d87b9b433c17121717ab3e6b32ba62b8
Elmer125 <elmermauricio05@hotmail.com>
2022-10-14 12:33:05 -0500

    Changes config production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
-    user_name: Rails.application.credentials.config[:SENDGRID_USERNAME],
-    password: Rails.application.credentials.config[:SENDGRID_PASSWORD],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'infinite-plateau-19302.herokuapp.com',
     enable_starttls_auto: true
   }
