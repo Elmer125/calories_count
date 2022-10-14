bd89df04c545d546fdb6b99beb92cdd1b2ae47e0
Elmer125 <elmermauricio05@hotmail.com>
2022-10-14 16:57:37 -0500

    Changes environment.rb and delete config in production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
-  password: ENV['SENDGRID_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
-  domain: 'infinite-plateau-19302.herokuapp.com',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
