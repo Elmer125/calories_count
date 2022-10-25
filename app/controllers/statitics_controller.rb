c573f181441d20ea95d3c62f77f4d959a58415c1
Elmer125 <elmermauricio05@hotmail.com>
2022-10-25 17:16:15 -0500

    Adds method validate_token to statitics_controller and before_action for validates token before graph view

--- app/controllers/statitics_controller.rb
+++ app/controllers/statitics_controller.rb
@@ -1,7 +1,9 @@
 class StatiticsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
+  before_action :validate_token, only: %i[validate graph]
+
   def graph
-    id = params[:user_id] || current_user.id
+    id = current_user&.id || @email_link.user_id
     @user = User.find(id)
     calories = Calorie.where(user_id: id).group(:burned_or_consumed)
     @calories_graph = calories.group_by_day(:created_at).sum(:calories_number)
@@ -26,14 +28,16 @@ class StatiticsController < ApplicationController
   end
 
   def validate
-    email_link = Statitic.where(token: params[:token]).where('expires_date > (?)', DateTime.now).first
+    return redirect_to new_user_session_path, alert: 'Link expired' unless @email_link
 
-    return redirect_to new_user_session_path, alert: 'Link expired' unless email_link
-
-    redirect_to statitics_path(user_id: email_link.user_id)
+    redirect_to statitics_path(nt: @email_link.token)
   end
 
   private
+
+  def validate_token
+    @email_link = Statitic.where(token: params[:nt]).where('expires_date > (?)', DateTime.now).first
+  end
 
   def statitic_params
     params.require(:statitic).permit(:email)
